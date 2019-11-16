ea8056a53c064fed4108fe7c6c5baf915a4d442e
jwwisniewski <j.w.wisniewski@gmail.com>
2019-11-16 21:45:18 +0100

    doctrine migrations and entity

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class ProductController extends AbstractController
+{
+    /**
+     * @Route("/product", name="create_product")
+     */
+    public function createProduct(EntityManagerInterface $entityManager): Response
+    {
+        $product = new Product();
+        $product->setTitle('Keyboard');
+        $product->setPrice(1999);
+        $product->setDescription('Ergonomic and stylish!');
+
+        $entityManager->persist($product);
+
+        $entityManager->flush();
+
+        return new Response('Saved new product with id ' . $product->getId());
+    }
+
+    /**
+     * @Route("/product/{id}", name="product_show")
+     */
+    public function show($id)
+    {
+        /** @var Product $product */
+        $product = $this->getDoctrine()
+            ->getRepository(Product::class)
+            ->find($id);
+
+        if (!$product) {
+            throw $this->createNotFoundException(
+                'No product found for id ' . $id
+            );
+        }
+
+        return new Response('Check out this great product: ' . $product->getTitle() . ' for ' . $product->getPrice());
+    }
+
+    /**
+     * @Route("/product/edit/{id}")
+     */
+    public function update($id)
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        /** @var Product $product */
+        $product = $entityManager->getRepository(Product::class)->find($id);
+
+        if (!$product) {
+            throw $this->createNotFoundException(
+                'No product found for id ' . $id
+            );
+        }
+
+        $product->setPrice($product->getPrice() + 20);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('product_show', [
+            'id' => $product->getId()
+        ]);
+    }
+}
